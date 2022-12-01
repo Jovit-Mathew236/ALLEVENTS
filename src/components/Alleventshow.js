bbf6e632bd6f984bab5ed4d13961b212dd3b66e2
Jovit-Mathew236 <jovitmathew236@gmail.com>
2022-12-01 11:55:55 +0530

    change styles

--- src/components/Alleventshow.js
+++ src/components/Alleventshow.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './../App.css'
-// import axios from "axios";
+import axios from "axios";
 import { Firebase } from '../firebase/config';
 import { useParams } from 'react-router';
 
@@ -30,12 +30,42 @@ const Alleventshow = () => {
     }, [Firebase, setInfo])
 
 
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://asthra.azba.in/_apiv2/event_details/all/0',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+            },
+            data: {
+                email: 'jovitmathew236@gmail.com',
+                code: 'A68FENW',
+                access: 'c90b6446-6a7c-11ed-a1eb-0242ac120002'
+            }
+        };
+
+        axios.request(options).then(function (response) {
+            // console.log(response.data);
+            setEventAPI(response.data)
+            // console.log(eventAPI);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }, [setEventAPI])
+
+
     return (
         <div>
             <div className="table_container">
+                <div className="head">
+                    <h1>{code}</h1>
+                    <p>{info.length}</p>
+                </div>
                 <table>
                     <thead>
                         <tr>
+                            <th>No</th>
                             <th>Name</th>
                             <th>Event</th>
                             <th>College</th>
@@ -49,6 +79,7 @@ const Alleventshow = () => {
                                 // console.log(data)
                                 return (
                                     <tr key={index}>
+                                        <td>{index+1}</td>
                                         <td>{data.name}</td>
                                         <td>{data.event}</td>
                                         <td>{data.college}</td>
